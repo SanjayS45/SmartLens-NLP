d18ea082fafecdd2263ca384a89202dc6d16cbc7
Sanjay Sundaram <sanjaysundaram45@gmail.com>
2023-07-05 14:53:45 -0500

    There is a bug with the word count displayed on the OLED, working on that...OLED now waits until user has read message and then clears display.

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,14 +1,16 @@
-
 from google.cloud import speech_v1p1beta1 as speech
 import os
 import serial
 import time
+import threading
+
 
 # defining serial object
 port = 'COM4'
 baud_rate = 9600
 ser = serial.Serial(port, baud_rate, timeout=1)
 time.sleep(2)
+
 
 # google cloud key
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/vsid3/Downloads/speechtotext-390719-614f9b327af3.json"
@@ -19,53 +21,73 @@ from nltk.tokenize import word_tokenize
 
 # Initialize the recognizer
 r = sr.Recognizer()
-global token, tokens_before_end
+global token
+exit_flag = True
 token = 0
-tokens_before_end = 'aaa'
-
+global check_done, check_deactivated
+check_done = False
+check_deactivated = False
 # Define a function to recognize speech
 def recognize_speech():
+    global exit_flag, tokens_before_end
     with sr.Microphone() as source:
-        while end_key_word != True:
-            print("Speak something...")
-            audio = r.listen(source)
+        print("Speak something...")
+        audio = r.listen(source)
+    try:
+        # Use Google Speech Recognition to convert speech to text
+        text = r.recognize_google(audio)
+        print("Recongized:", text)
+        #ser.write(text.encode())
+        # Tokenize the recognized text using NLTK
+        global tokens
+        tokens = word_tokenize(text)
+        
+        # Prints messages to the OLED until the user wants to stop the conversation.
+        start_key_word()
+        end_key_word()
+        if check_deactivated == False and check_done == True and end_key_word() == False:
+            ser.write(' '.join(tokens).encode())
+        elif end_key_word() == True:
+            ser.write(tokens_before_end.encode())
+            ser.write("\n\nVoice Recognition is shutting down...".encode())
+        
             
-            try:
-                # Use Google Speech Recognition to convert speech to text
-                text = r.recognize_google(audio)
-                #print("Recognized speech:", text)
-                # Tokenize the recognized text using NLTK
-                global tokens
-                tokens = word_tokenize(text)
-                print("Tokens:", tokens)
-                
-                # Keeps the serial open until the user wants to stop the conversation.
-            
-                ser.write(text.encode())
-                #recognize_speech()
-                
-            except sr.WaitTimeoutError:
-                continue
-            except sr.UnknownValueError:
-                print("Speech recognition could not understand audio.")
-        ser.write(tokens_before_end.encode())
-        ser.close()
-
+    except sr.UnknownValueError:
+        print("Speech recognition could not understand audio.")
 
 def end_key_word():
+    global tokens_before_end, check_deactivated, check_done
     end = False
     tokens_before_end = ''
     for token in tokens:
-        if token == 'end':
+        if token == 'deactivate':
+            check_deactivated = True
+            check_done = False
             return True
         else:
             tokens_before_end += token + ' '
-    return end        
-    
-    
-# Call the function to start speech recognition
+            continue
+    return end      
 
-recognize_speech()
+def start_key_word():
+    global check_done, check_deactivated, tokens
+    end_start = False
+    index = 0
+    for token in tokens:
+        if token == 'activate':
+            index = tokens.index(token)
+            check_deactivated = False
+            if check_done == False:
+                tokens = tokens[index+1:]
+                check_done = True
+            return True
+        else:
+            continue
+    return end_start   
 
-
-
+def listener_thread_function():
+   while exit_flag == True:
+        recognize_speech()
+        
+listener_thread = threading.Thread(target=listener_thread_function)
+listener_thread.start()
